d28a6e583553ff45148b38e55068b27ed700eb9d
Ericwiz <wisdomagu44@gmail.com>
2022-10-06 11:54:23 +0100

    setup completed

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
 import { tweetsData } from "./data.js";
- console.log(tweetsData)
+
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetsData)
+})
+
+function getFeedHtml() {
+    let feedHtml = ''
+    tweetsData.forEach(data => {
+        feedHtml += `
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${data.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${data.handle}</p>
+                    <p class="tweet-text">${data.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">${data.replies.length}</span>
+                        <span class="tweet-detail">${data.likes}</span>
+                        <span class="tweet-detail">${data.retweets}</span>
+                    </div>   
+                </div>            
+            </div>
+       </div>
+        `
+        console.log(feedHtml)
+        return feedHtml
+    })
+     
+}
+getFeedHtml()
